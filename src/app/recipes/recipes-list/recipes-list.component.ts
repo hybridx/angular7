6cbb850e5b1a1cdc55f99d33f66f755de946f040
Deepesh Nair <hybridx18@gmail.com>
2019-06-20 22:08:42 +0530

    Basic layout for the project

---
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.css']
+})
+export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] =[
+    new Recipe('Pizza Recipe',
+    "A great Pizza",
+    "https://upload.wikimedia.org/wikipedia/commons/0/0b/Recipe_Unlimited_logo.png")
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
