782aca6837b107ffdafd0a543b81ec79aa666878
Deepesh Nair <hybridx18@gmail.com>
2019-06-20 07:40:58 +0530

    Data binding example complete

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStaus = 'No server was created!';
+  serverName = 'TestServer';
+  serverCreated = false;
+  servers = ['TestServer','TestServer 2'];
 
-  constructor() { }
+  constructor() { 
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStaus = 'Server was created! name is ' + this.serverName;
+    this.servers.push(this.serverName);
+    this.serverCreated = true;
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
