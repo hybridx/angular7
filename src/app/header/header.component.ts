9f634136e15e3193c4fa012dc402c2b7236bb8d7
Deepesh Nair <denair@redhat.com>
2019-09-19 20:02:34 +0530

    [home-routing] - Used event emitter to resemble routing

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,19 @@
-import { Component } from "@angular/core";
+import { Component, Output, EventEmitter } from "@angular/core";
 
 @Component({
     selector: 'app-header',
     templateUrl: 'header.component.html'
 })
 export class HeaderComponent {
+
+  @Output() recipe:EventEmitter<any> = new EventEmitter<any>();
+
+  showRecipes() {
+    this.recipe.emit(true);
+  }
+
+  showShopping() {
+    this.recipe.emit(false);
+  }
 
 }
