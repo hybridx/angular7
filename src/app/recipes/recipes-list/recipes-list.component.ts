c47a512bf3308dbecfebafe10fba7c3f96b0c486
Deepesh Nair <denair@redhat.com>
2019-09-21 02:01:07 +0530

    [recipe-list] - Refactored using @Input

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -7,10 +7,14 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+
   recipes: Recipe[] =[
     new Recipe('Pizza Recipe',
     "A great Pizza",
-    "https://upload.wikimedia.org/wikipedia/commons/0/0b/Recipe_Unlimited_logo.png") 
+    "https://upload.wikimedia.org/wikipedia/commons/0/0b/Recipe_Unlimited_logo.png"),
+    new Recipe('Another Pizza Recipe',
+    "Another great Pizza",
+    "https://upload.wikimedia.org/wikipedia/commons/0/0b/Recipe_Unlimited_logo.png")
   ];
 
   constructor() { }
